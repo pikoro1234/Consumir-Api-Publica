4dbc4e2d693614694e5b49f7506cd4e6b7aa9767
jorge <donjuan.jfc@gmail.com>
2020-11-06 00:27:12 +0100

    get datos ajax y creacion funcion llenar con console

--- Materialprojecte/js/pokemonAjax.js
+++ Materialprojecte/js/pokemonAjax.js
@@ -1,3 +1,30 @@
 $("#lista-pokemon").click(function(){
-    alert("boton lista");
+
+    let pokemons = [];
+
+    //ajaxPokemon
+    let urlPokemons = "js/data/pokemon.json";
+    
+	$.ajax({	
+
+        async: false,        
+		url: urlPokemons
+	})
+	.done(function(data){
+        
+        llenarPokemons(data);
+    });//fin ajax
+    
 });//fin click lista pokemon
+
+
+const llenarPokemons = (datos) =>{
+
+    pokemons = datos.pokemon;
+
+    for(let poke of pokemons){
+
+        console.log(poke.name)
+    }
+}
+
