fc090dcc3c38d23ba139ba42c5bc253f2a52af83
jorge <donjuan.jfc@gmail.com>
2020-11-09 15:50:41 +0100

    creacion clase para guardar json en objetc individuales

---
+++ Materialprojecte/js/pokemonObject.js
@@ -0,0 +1,13 @@
+class Pokemon {
+
+    constructor(id, nombre, peso, imagen) {
+
+        this.id = id;
+
+        this.nombre = nombre;
+
+        this.peso = peso;
+
+        this.imagen = imagen;
+    }
+}
